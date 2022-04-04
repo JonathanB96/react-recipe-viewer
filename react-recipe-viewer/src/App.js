026c5b9b174e508918a21de72b83c3cac3122cf2
jonathan bola <bolajonathan@yahoo.com>
2022-04-04 08:37:47 -0500

    returning other components

--- react-recipe-viewer/src/App.js
+++ react-recipe-viewer/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Home from './Home';
 import Header from './Header';
+import Signup from './Signup';
+import Addrecipe from './AddRecipe';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Home/>
+      <Login/>
+      <Signup />
+      <Addrecipe />
+
     </div>
   );
 }
