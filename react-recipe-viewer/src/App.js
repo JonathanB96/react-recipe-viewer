679dbf97611bc645fa6fed1761c7fc7b2c1a0a96
jonathan bola <bolajonathan@yahoo.com>
2022-04-04 08:45:14 -0500

    adding routes

--- react-recipe-viewer/src/App.js
+++ react-recipe-viewer/src/App.js
@@ -2,17 +2,28 @@ import './App.css';
 import Home from './Home';
 import Header from './Header';
 import Signup from './Signup';
-import Addrecipe from './AddRecipe';
+import AddRecipe from './AddRecipe';
 import Login from './Login';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <Login/>
-      <Signup />
-      <Addrecipe />
+      <Header />
+      <Switch>
+        <Route exact path="/signup">
+          <Signup/>
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/addrecipe">
+          <AddRecipe />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
 
     </div>
   );
